c4527eed17e1515b17f14cfd42af33d665d79df9
Peter Jackson <Pete@Ping.local>
2008-05-10 02:43:56 -0400

    Adding support for custom title, proect limit, and the "show all" link name

--- lib/github_sidebar.rb
+++ lib/github_sidebar.rb
@@ -2,5 +2,9 @@ class GitHubSidebar < Sidebar
   display_name "GitHub"
   description "Show your github projects using Dr Nic's Github Badge"
 
+  setting :title, "My projects"
   setting :github_username, "username"
+  setting :limit, 10
+  setting :show_link, "Show all"
+  
 end
