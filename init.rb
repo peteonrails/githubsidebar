f19885f902e28c1050e190eaa69eab1b31687535
Peter Jackson <Pete@Ping.local>
2008-05-10 01:23:43 -0400

    Initial Import

---
+++ init.rb
@@ -0,0 +1,5 @@
+require 'sidebar'
+require 'github_sidebar'
+
+GitHubSidebar.view_root = File.dirname(__FILE__) + '/views'
+
---
+++ lib/github_sidebar.rb
@@ -0,0 +1,6 @@
+class GitHubSidebar < Sidebar
+  display_name "GitHub"
+  description "Show your github projects using Dr Nic's Github Badge"
+
+  setting :github_username, "username"
+end
