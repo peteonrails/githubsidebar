6f3fcfe44fa4712ac4f1d4f8030a6e09034aac2d
Peter Jackson <Pete@Ping.local>
2008-05-13 02:42:16 -0400

    Adding support for YAGB badge.

--- lib/github_sidebar.rb
+++ lib/github_sidebar.rb
@@ -1,10 +1,12 @@
 class GitHubSidebar < Sidebar
   display_name "GitHub"
-  description "Show your github projects using Dr Nic's Github Badge"
+  description "Show your github projects using one of the popular Github Badges"
+
+  setting :style, "Badge Type", :input_type => :radio,
+          :choices => [["drnic",  "Dr Nic"], ["yagb", "YAGB"]]
 
   setting :title, "My projects"
   setting :github_username, "username"
   setting :limit, 10
   setting :show_link, "Show all"
-  
 end
